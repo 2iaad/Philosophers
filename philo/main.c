09de256fa6179f39920d4942fe5b9842d5e9a109
Ziad Derfoufi <ziadderfoufi@Ziads-MacBook-Pro.local>
2024-04-02 12:25:24 +0000

    parsing

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 06:59:47 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/04/01 10:29:34 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/04/02 12:23:46 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int main(int ac, char **av)
 {
-    if (ac >= 2 && ac <= 5)
+    if (ac == 5 || ac == 6)
     {
         parsing(ac, av);
     }
+    else
+        printf("Wrong number of arguments\n");
 }
--- philo/parsing.c
+++ philo/parsing.c
@@ -6,34 +6,55 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 07:07:46 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/04/01 10:49:44 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/04/02 12:24:15 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-bool 	f(char *av)
+bool 	pure_numbers(char **av)
 {
 	int i;
+	int j;
 
 	i = -1;
-
 	while (av[++i])
-		if (!(av[i] >= '0' && av[i] <= '9'))
+	{
+		j = -1;
+		while (av[i][++j])
 		{
-			return (false);
+			if ((av[i][j] =='+' || av[i][j] == '-') && !j) // '-' in case of -0
+				j++;
+			if (!(av[i][j] >= '0' && av[i][j] <= '9'))
+				return (false);
 		}
+		if(ft_atol(av[i]) < 0)
+			return(false);
+	}
 	return (true);
 }
 
 void    parsing(int ac, char **av)
 {
 	t_philo	philo;
-		
-	philo.n_philo = ft_atol(av[1]);
-	philo.time_to_die = ft_atol(av[2]);
-	philo.time_to_eat = ft_atol(av[3]);
-	philo.time_to_sleep = ft_atol(av[4]);
-	if (ac == 5)
-		philo.meals = ft_atol(av[5]);
+
+	if (!pure_numbers(av + 1))
+	{
+		printf("PARSER\n");
+		exit(1);
+	}
+	else
+		printf("NICE !!\n");
+
+	// printf("%d\n",ft_atol(av[1]));
+	// printf("%d\n",ft_atol(av[2]));
+	// printf("%d\n",ft_atol(av[3]));
+	// printf("%d\n",ft_atol(av[4]));
+
+	// philo.n_philo = ft_atol(av[1]);
+	// philo.time_to_die = ft_atol(av[2]);
+	// philo.time_to_eat = ft_atol(av[3]);
+	// philo.time_to_sleep = ft_atol(av[4]);
+	// if (ac == 5)
+	// 	philo.meals = ft_atol(av[5]);
 }
