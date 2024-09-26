e0962218cd4e0dea6d82f690d7df0f30c84927f0
Ziyad Derfoufi <zderfouf@e1r4p4.1337.ma>
2024-09-26 09:50:56 +0100

    [norm] +added a header to the Makefile

--- philo/main.c
+++ philo/main.c
@@ -6,22 +6,22 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 06:59:47 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/09/25 12:08:08 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/09/26 09:49:20 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_philo *philo;
+	t_philo	*philo;
 
-    if (ac == 5 || ac == 6)
-    {
-        parsing(ac, av);
-        init_data(ac, av, &philo);
+	if (ac == 5 || ac == 6)
+	{
+		parsing(ac, av);
+		init_data(ac, av, &philo);
 		create_philo(philo);
-    }
-    else
-        printf("Wrong number of arguments\n");
+	}
+	else
+		printf("Wrong number of arguments\n");
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,52 +6,50 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 07:00:11 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/09/25 15:38:54 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/09/26 09:47:48 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-#include <sys/_pthread/_pthread_mutex_t.h>
-#include <sys/_pthread/_pthread_t.h>
-#include <time.h>
-#include <sys/time.h>
-#include <stdio.h>
-#include <pthread.h> // contain all functions that control threads and mutexes
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdbool.h>
+# include <time.h>
+# include <sys/time.h>
+# include <stdio.h>
+# include <pthread.h> // contain all functions that control threads and mutexes
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
 
 typedef struct s_philo
 {
-    pthread_t		th;
-    int				id;
+	pthread_t		th;
+	int				id;
 	int				meals_eaten;
 	size_t			last_meal;
-    pthread_mutex_t	*l_fork;
-    pthread_mutex_t	*r_fork;
-	struct	s_table	*table;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
+	struct s_table	*table;
 }		t_philo;
 
 typedef struct s_table
 {
 	pthread_t		guard;
 	size_t			start_time;
-    int				n_philos; // also number of forks
-    int				time_to_die; // time li yqdr lphilo ytsna mn last meal ola last simulation deyalo
-    int				time_to_eat; // time li yakhod lphilo bach ysali lmakla, atkoun khasah 2 forks
-    int				time_to_sleep; // The time a philosopher will spend sleeping
-    int				n_meals;
+	int				n_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				n_meals;
 	bool			death_flag;
-	pthread_mutex_t print;
-	pthread_mutex_t last_meal_m;
-	pthread_mutex_t n_meals_m;
-	pthread_mutex_t death_m;
+	pthread_mutex_t	print;
+	pthread_mutex_t	last_meal_m;
+	pthread_mutex_t	n_meals_m;
+	pthread_mutex_t	death_m;
 	pthread_mutex_t	*forks;
 }		t_table;
 
-int     ft_atol(char *s);
+int		ft_atol(char *s);
 void	print(t_philo *philo, char *str);
 size_t	get_current_time(void);
 int		ft_usleep(size_t milliseconds);
@@ -61,14 +59,14 @@ void	forks_lock(t_philo *philo);
 void	forks_unlock(t_philo *philo);
 void	mutex_destroy(t_philo *philo);
 
-void    parsing(int ac, char **av);
+void	parsing(int ac, char **av);
 void	init_data(int ac, char **av, t_philo **philo);
-void    create_philo(t_philo *philo);
+void	create_philo(t_philo *philo);
 
 /*         ACTIONS           */
 
-void    die(t_table *table);
-bool    eat(t_philo *philo);
-bool    to_sleep(t_philo *philo);
+void	die(t_table *table);
+bool	eat(t_philo *philo);
+bool	to_sleep(t_philo *philo);
 
-# endif
+#endif
